236497ad52ed624c6c1af3eb0a0eba42579b415a
CollinKrasnai <cmk6785@psu.edu>
2025-11-02 19:32:28 -0500

    Create web-dev-server.config.mjs

---
+++ web-dev-server.config.mjs
@@ -0,0 +1,23 @@
+// import { hmrPlugin, presets } from '@open-wc/dev-server-hmr';
+
+/** Use Hot Module replacement by adding --hmr to the start command */
+const hmr = process.argv.includes('--hmr');
+
+export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
+  open: 'demo/index.html', // Open the demo page by default
+  watch: !hmr,
+  /** Resolve bare module imports */
+  nodeResolve: {
+    exportConditions: ['browser', 'development'],
+  },
+
+  /** Set appIndex to enable SPA routing */
+  appIndex: 'demo/index.html',
+
+  plugins: [
+    /** Use Hot Module Replacement by uncommenting. Requires @open-wc/dev-server-hmr plugin */
+    // hmr && hmrPlugin({ exclude: ['**/*/node_modules/**/*'], presets: [presets.litElement] }),
+  ],
+
+  // See documentation for all available options
+});
